623826dc79b7c63c6ec20bcc17e9f3feb58a699b
tuf-ubuntu <2514034568@qq.com>
2025-06-03 02:00:11 +0800

    add key and vivado io pin table

--- 3_key/key_filter.v
+++ 3_key/key_filter.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 06/03/2025 01:13:12 AM
+// Create Date: 06/03/2025 01:16:24 AM
 // Design Name: 
-// Module Name: key
+// Module Name: key_filter
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -18,43 +18,41 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
+
 `timescale 1ns/1ns
-module key #(
+module key_filter #(
     parameter CNT_MAX = 20'd999_999
 ) (
     input wire  sys_clk,
     input wire  sys_rst_n,
     input wire  key_in,
 
-    output reg  led
+    output reg  key_flag
     // output  reg led
 );
-wire key_flag;
 
-
-
-key_filter #(
-    .CNT_MAX(20'd999_999)
-)
-key_filter1
-(
-    .sys_clk  (sys_clk),
-    .sys_rst_n(sys_rst_n),
-    .key_in   (key_in),
-
-    .key_flag  (key_flag)
-    // output  reg led
-);
+reg [19:0] cnt_20ms;
 
 always @(posedge sys_clk or negedge sys_rst_n) begin
-    if(!sys_rst_n) 
-        led <= 1'b0;
-    else if(key_flag == 1'b1)
-        led <= ~led;
-    else
-        led <= led;
+    if(sys_rst_n == 1'b0)
+        cnt_20ms <= 20'd0;
+    else if(key_in == 1'b1)
+        cnt_20ms <= 20'd0;
+    else if(cnt_20ms == CNT_MAX && key_in == 1'b0)
+        cnt_20ms <= cnt_20ms;
+    else 
+        cnt_20ms <= cnt_20ms + 1'b1;
+    
 end
 
+always @(posedge sys_clk or negedge sys_rst_n) begin
+    if(sys_rst_n == 0)
+        key_flag <= 0;
+    else if(cnt_20ms ==CNT_MAX -1)
+        key_flag <= 1;
+    else
+        key_flag <= 0;
+end
+
+
 endmodule
-
-
---
+++ vivado_constrains/constrains.xdc
@@ -0,0 +1,4 @@
+set_property -dict {PACKAGE_PIN U18 IOSTANDARD LVCMOS33} [get_ports sys_clk]
+set_property -dict {PACKAGE_PIN N16 IOSTANDARD LVCMOS33} [get_ports sys_rst_n]
+set_property -dict {PACKAGE_PIN H15 IOSTANDARD LVCMOS33} [get_ports {led[0]}]
+set_property -dict {PACKAGE_PIN L15 IOSTANDARD LVCMOS33} [get_ports {led[1]}]
